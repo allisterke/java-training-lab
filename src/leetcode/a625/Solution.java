fd1a86fa01d3d307da754f6cf6cfd04aa3628739
ally <allisterke@gmail.com>
2017-06-18 12:47:53 +0800

    add another method from left to right

--- src/leetcode/a625/Solution.java
+++ src/leetcode/a625/Solution.java
@@ -2,13 +2,34 @@ package leetcode.a625;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Solution {
     public int smallestFactorization(int a) {
         return smallestFactorization(a, 0);
     }
 
-    public int smallestFactorization(int a, int d) {
+    int smallestFactorization1(int a, int d) {
+        if(a < 10) {
+            long c = d * 10L + a;
+            return c > Integer.MAX_VALUE ? 0 : (int)c;
+        }
+        int f = 0;
+        for(int n = 2; n <= 9; ++ n) {
+            if(a % n == 0) {
+                long k = d * 10L + n;
+                if (k <= Integer.MAX_VALUE) {
+                    int m = smallestFactorization1(a / n, (int)k);
+                    if(m != 0 && (f == 0 || m < f)) {
+                        f = m;
+                    }
+                }
+            }
+        }
+        return f;
+    }
+
+    int smallestFactorization(int a, int d) {
         if(d >= 11) {
             return 0;
         }
@@ -40,10 +61,18 @@ public class Solution {
         List results = Arrays.asList();
 
         Solution s = new Solution();
-        for(int i = 0; i < tests.size(); ++ i) {
+//        for(int i = 0; i < tests.size(); ++ i) {
+//            System.out.print("case ");
+//            System.out.println(i);
+//            System.out.println(s.smallestFactorization(tests.get(i), 0));
+//            System.out.println(s.smallestFactorization1(tests.get(i), 0));
+//        }
+        for(int i = 0; i < 10; ++ i) {
+            int n = new Random().nextInt((int)1e4);
             System.out.print("case ");
             System.out.println(i);
-            System.out.println(s.smallestFactorization(tests.get(i)));
+            System.out.println(s.smallestFactorization(n, 0));
+            System.out.println(s.smallestFactorization1(n, 0));
         }
     }
 }
