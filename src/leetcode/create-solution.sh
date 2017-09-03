1a3fce0cbbdd005027d57efecb2c10a8514cd895
Ke Yangbin <keyangbin@126.com>
2017-09-03 11:18:25 +0800

    add support to multiple solution creation for bash expansion {xxx..yyy}

--- src/leetcode/create-solution.sh
+++ src/leetcode/create-solution.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 1 ]; then
     echo 'please input package name'
     exit 1
 fi
 
-DIR=$1
-if [[ ! $DIR =~ a.* ]]; then
-    DIR=$(printf "a%03d" $DIR)
-fi
+for DIR in $*; do
+    if [[ ! $DIR =~ a.* ]]; then
+        DIR=$(printf "a%03d" $DIR)
+    fi
+    
+    if [ -e $DIR ]; then
+        echo $DIR exists
+        continue
+    fi
 
-mkdir $DIR
-if [ $? -ne 0 ]; then
-    exit 1
-fi
-
-cp Solution.java $DIR/
-sed -i -r "1 s/^(.*)$/package leetcode.$DIR;/" $DIR/Solution.java
-
-echo $DIR/Solution.java created
+    mkdir $DIR 
+    cp Solution.java $DIR/
+    sed -i -r "1 s/^(.*)$/package leetcode.$DIR;/" $DIR/Solution.java
+    
+    echo $DIR/Solution.java created
+done
