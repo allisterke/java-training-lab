774690c1b2d1b24034f2926f0f68ea7e2699b300
Ke Yangbin <keyangbin@126.com>
2018-01-02 15:54:30 +0800

    dfs

--- src/leetcode/a756/Solution.java
+++ src/leetcode/a756/Solution.java
@@ -3,15 +3,37 @@ package leetcode.a756;
 import java.util.*;
 
 public class Solution {
+
+    private Map<Integer, Set<Character>> transitionMap;
+
     public boolean pyramidTransition(String bottom, List<String> allowed) {
-        Map<Integer, Set<Character>> map = new HashMap<>();
-        for(String s : allowed) {
-            int key = getKey(s.charAt(0), s.charAt(1));
-            if(!map.containsKey(key)) {
-                map.put(key, new HashSet<>());
-            }
-            map.get(key).add(s.charAt(2));
+        transitionMap = getTransitionMap(allowed);
+        return dfs(bottom, new StringBuilder(), 0);
+    }
+
+    boolean dfs(String bottom, StringBuilder up, int index) {
+        if(bottom.length() == 1) {
+            return true;
         }
+        if (index == bottom.length() - 1) {
+            return dfs(up.toString(), new StringBuilder(), 0);
+        }
+        int key = getKey(bottom.charAt(index), bottom.charAt(index + 1));
+        if(!transitionMap.containsKey(key)) {
+            return false;
+        }
+        for (char c : transitionMap.get(key)) {
+            up.append(c);
+            if(dfs(bottom, up, index+1)) {
+                return true;
+            }
+            up.setLength(up.length() - 1);
+        }
+        return false;
+    }
+
+    public boolean pyramidTransition0(String bottom, List<String> allowed) {
+        Map<Integer, Set<Character>> map = getTransitionMap(allowed);
         List<Collection<Character>> current = new ArrayList<>();
         for (int i = 0; i < bottom.length(); ++ i) {
             current.add(Arrays.asList(bottom.charAt(i)));
@@ -34,6 +56,18 @@ public class Solution {
             current = next;
         }
         return !current.isEmpty();
+    }
+
+    private Map<Integer, Set<Character>> getTransitionMap(List<String> allowed) {
+        Map<Integer, Set<Character>> map = new HashMap<>();
+        for(String s : allowed) {
+            int key = getKey(s.charAt(0), s.charAt(1));
+            if(!map.containsKey(key)) {
+                map.put(key, new HashSet<>());
+            }
+            map.get(key).add(s.charAt(2));
+        }
+        return map;
     }
 
     int getKey(char a, char b) {
